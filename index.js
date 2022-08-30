e9aed174b060c6b0a3e19a89f5d71a582c579121
Vinh1312 <tuanvinhnguyen1312@gmail.com>
2022-08-30 12:44:32 +0200

    Initialization

---
+++ index.js
@@ -0,0 +1,32 @@
+let countHome =0
+let scoreHome = document.getElementById("score-home")
+
+
+function add1home(){
+    countHome +=1
+    scoreHome.textContent =  countHome
+}
+function add2home(){
+    countHome +=2
+    scoreHome.textContent =  countHome
+}
+function add3home(){
+    countHome +=3
+    scoreHome.textContent =  countHome
+}
+
+let countGuest = 0
+let scoreGuest = document.getElementById("score-guest")
+
+function add1guest(){
+    countGuest +=1
+    scoreGuest.textContent =  countGuest
+}
+function add2guest(){
+    countGuest +=2
+    scoreGuest.textContent =  countGuest
+}
+function add3guest(){
+    countGuest +=3
+    scoreGuest.textContent =  countGuest
+}
